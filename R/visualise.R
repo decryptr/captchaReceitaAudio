a3a07a665c8152394c2a6ee99345e0407ec3bfed
jtrecenti <julio.trecenti@gmail.com>
2016-10-22 16:17:43 -0200

    coloquei todos os métodos de visualização no mesmo arquivo

---
+++ R/visualise.R
@@ -0,0 +1,48 @@
+#' Visualizar imagem do captcha.
+#'
+#' Mostra o arquivo PNG do captcha.
+#'
+#' Funciona bem com o objeto retornado pela funcao \code{\link{baixar_imgs_audios}}: \code{<obj>$imagem$request$output$path}
+#'
+#' @param arq_png character. caminho/do/png/do/captcha.png que se deseja visualizar.
+#'
+#' @export
+visualizar_imagem <- function(arq_png) {
+  img <- arq_png %>%
+    arquivo() %>%
+    png::readPNG()
+  grid::grid.newpage()
+  grid::grid.raster(img)
+}
+
+#' Plota onda sonora com os cortes identificados.
+#'
+#' Gera um grafico da onda sonora com as partes identificadas como letras em destaque.
+#' Serve para verificar se as 6 letras foram identificadas corretamente.
+#'
+#' @param df_com_letras_identificadas objeto data.frame retornado pela funcao \code{\link{identificar_letras}}.
+#'
+#' @export
+visualizar_corte <- function(df_com_letras_identificadas) {
+  ggplot(df_com_letras_identificadas %>%
+           mutate(eh_letra = final_diff %% 2 != 0), aes(x = tempo)) +
+    geom_line(aes(y = scale(som), alpha = eh_letra), show.legend = FALSE) +
+    geom_line(aes(y = scale(w0), colour = "w0"), show.legend = FALSE) +
+    geom_line(aes(y = scale(final), colour = "final"), show.legend = FALSE) +
+    geom_line(aes(y = scale(final_diff), colour = "final_diff"), show.legend = FALSE) +
+    theme_minimal() +
+    theme(axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          panel.grid = element_blank()) +
+    labs(x = "", y = "")
+}
+
+
+#' Aplicacao Shiny Classificador de Captchas
+#'
+#'@export
+app_classificador <- function(arq = NULL) {
+  if (is.null(arq)) arq <- system.file("classificador/app.R",
+                                       package = 'captchaReceitaAudio')
+  shiny::runApp(arq)
+}
