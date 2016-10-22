a09bc11aaabc14b280952117415d34ac4331ca59
jtrecenti <julio.trecenti@gmail.com>
2016-10-22 16:17:23 -0200

    adicionada S3 method "arquivo" para pegar o caminho caso seja uma response

---
+++ R/utils.R
@@ -0,0 +1,23 @@
+#' Pipe operator
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+
+arquivo <- function(x) {
+  UseMethod('arquivo')
+}
+
+arquivo.response <- function(x) {
+  arq <- x$request$output$path
+  if (!file.exists(arq)) stop('Arquivo não encontrado.')
+  arq
+}
+
+arquivo.character <- function(x) {
+  if (!file.exists(x)) stop('Arquivo não encontrado.')
+  x
+}
